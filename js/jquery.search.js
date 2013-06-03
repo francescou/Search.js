3aa8b80798ab6cd6bdd014b55e01e3f2b3902b7a
Francesco Uliana <francesco@uliana.it>
2013-06-03 20:43:06 +0100

    fix retrieval problem caused by wrong sorting of strings

--- js/jquery.search.js
+++ js/jquery.search.js
@@ -1,68 +1,69 @@
+/*jslint browser:true*/
+/*global Search,jQuery*/
 /*
  *  Project: search.js
- *  Description: 
+ *  Description:
  *  Author: Francesco Uliana
- *  License: 
+ *  License:
  *
  *  depends on Search.js
  */
-;
-(function ($, window, undefined) {
-    
-    var pluginName = 'searchjs',
-        document = window.document,
-        defaults = {
-			logger: true,
-			engine: {
-				stopWords: ["and", "or", "if", "else"],
-				minLength: 2, //minimum keyword length
-				stem: true
-			},
-			display: function(data){
-				if(console){
-					console.log(data);
-				}
-			}
-        };
+(function ($, window) {
+  "use strict";
 
-    function Plugin( element, options ) {
-        this.element = element;
-
-        this.options = $.extend( {}, defaults, options) ;
-        
-        this._defaults = defaults;
-        this._name = pluginName;
-        
-        this.init();
-    }
-
-    Plugin.prototype.init = function () {
-
-		var options = this.options;
-		var element = $(this.element);
-		
-		Search.init(options.engine, options.content, function () {
-			element
-			.attr("disabled", false)
-			.on("keyup", function () {
-				var query = $(this).val();
-				if(query.length < options.engine.minLength) {
-					options.display([]);
-				} else {
-					Search.search(query, function (results) {
-						options.display(results);
-					});
-				}
-			});
-		});
+  var pluginName = 'searchjs',
+    defaults = {
+      engine: {
+        logger: true,
+        stopWords: ["and", "or", "if", "else"],
+        minLength: 2, //minimum keyword length
+        stem: true
+      },
+      display: function (data) {
+        if (window.console) {
+          window.console.log(data);
+        }
+      }
     };
 
-    $.fn[pluginName] = function ( options ) {
-        return this.each(function () {
-            if (!$.data(this, 'plugin_' + pluginName)) {
-                $.data(this, 'plugin_' + pluginName, new Plugin( this, options ));
-            }
+  function Plugin(element, options) {
+    this.element = element;
+
+    this.options = $.extend({}, defaults, options);
+
+    this._defaults = defaults;
+    this._name = pluginName;
+
+    this.init();
+  }
+
+  Plugin.prototype.init = function () {
+
+    var options = this.options,
+      element = $(this.element);
+
+    Search.init(options.engine, options.content, function () {
+      element
+        .attr("disabled", false)
+        .on("keyup", function () {
+          var query = $(this).val();
+          if (query.length < options.engine.minLength) {
+            options.display([]);
+          } else {
+            Search.search(query, function (results) {
+              options.display(results);
+            });
+          }
         });
-    };
+    });
+  };
+
+  $.fn[pluginName] = function (options) {
+    return this.each(function () {
+      if (!$.data(this, 'plugin_' + pluginName)) {
+        $.data(this, 'plugin_' + pluginName, new Plugin(this, options));
+      }
+    });
+  };
 
 }(jQuery, window));
